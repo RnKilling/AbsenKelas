0b99ebd108dd4e6a6e5c4ea22793b2bf90b28f46
Sihil12 <sihilmongbu@gmail.com>
2024-07-17 22:59:35 +0700

    menghapus file

--- admin/pegawaihapus.php
+++
@@ -1,4 +0,0 @@
-<?php
-$koneksi->query("DELETE FROM pengguna WHERE id='$_GET[id]'");
-echo "<script>alert('Data Berhasil Di Hapus');</script>";
-echo "<script>location='index.php?halaman=mahasiswadaftar';</script>";
