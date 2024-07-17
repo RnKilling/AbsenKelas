58df2b910dd7282a6759408b47f2fe92161b160c
Rnkilling <anggararivan9@gmail.com>
2024-07-17 15:39:13 +0700

    mengganti logo pada header dan index

--- header.php
+++ header.php
@@ -105,7 +105,7 @@ function getBulan($bln)
     </div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-warning py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
       <a href="index.php" class="navbar-brand ms-4 ms-lg-0">
-        <h1 class="fw-bold text-primary m-0"><img src="foto/logo.png" style="border-radius:10px;width:100px"><span class="text-white" style="padding-left:25px;">E - Absensi</span></h1>
+        <h1 class="fw-bold text-primary m-0"><img src="Poltek_GT.png" style="border-radius:10px;width:100px"><span class="text-white" style="padding-left:25px;">E - Absensi</span></h1>
       </a>
       <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
