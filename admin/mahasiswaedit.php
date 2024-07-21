2a8e31b3428b06a0aa22b20ba6b7636f9780d107
Rnkilling <anggararivan9@gmail.com>
2024-07-21 14:12:16 +0700

    menambahkan addslashes pada code

--- admin/mahasiswaedit.php
+++ admin/mahasiswaedit.php
@@ -7,21 +7,21 @@ if ($_SESSION['admin']['level'] != "Admin") {
 <h3>Edit Mahasiswa</h3>
 <br>
 <?php
-$ambil = $koneksi->query("SELECT * FROM pengguna WHERE id='$_GET[id]'");
+$ambil = $koneksi->query("SELECT * FROM pengguna WHERE id='" . addslashes($_GET['id']) . "'");
 $row = $ambil->fetch_assoc();
 ?>
 <form method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label>NIM</label>
-        <input value="<?php echo $row['nip']; ?>" type="number" value="" class="form-control" name="nip">
+        <input value="<?php echo addslashes($row['nip']); ?>" type="number" value="" class="form-control" name="nip">
     </div>
     <div class="form-group">
         <label>Nama</label>
-        <input value="<?php echo $row['nama']; ?>" type="text" value="" class="form-control" name="nama">
+        <input value="<?php echo addslashes($row['nama']); ?>" type="text" value="" class="form-control" name="nama">
     </div>
     <div class="form-group">
         <label>Email</label>
-        <input value="<?php echo $row['email']; ?>" type="email" class="form-control" name="email">
+        <input value="<?php echo addslashes($row['email']); ?>" type="email" class="form-control" name="email">
     </div>
     <div class="form-group">
         <label>Jabatan</label>
@@ -33,7 +33,7 @@ $row = $ambil->fetch_assoc();
     </div>
     <div class="form-group">
         <label>Telepon</label>
-        <input value="<?php echo $row['telepon']; ?>" type="number" class="form-control" name="telepon">
+        <input value="<?php echo addslashes($row['telepon']); ?>" type="number" class="form-control" name="telepon">
     </div>
     <div class="form-group">
         <label>Password</label>
@@ -42,8 +42,8 @@ $row = $ambil->fetch_assoc();
     </div>
     <div class="form-group">
         <label>Alamat</label>
-        <textarea value="<?php echo $row['alamat']; ?>" class="form-control" name="alamat" id="alamat" rows="10">
-        <?php echo $row['alamat']; ?>
+        <textarea class="form-control" name="alamat" id="alamat" rows="10">
+            <?php echo addslashes($row['alamat']); ?>
         </textarea>
         <script>
             CKEDITOR.replace('alamat');
@@ -55,26 +55,29 @@ $row = $ambil->fetch_assoc();
             <input type="file" class="form-control" name="foto">
         </div>
     </div>
-    <button class="btn btn-primary" name="ubah"><i class="glyphicon glyphicon-saved"></i>Simpan</a></button>
+    <button class="btn btn-primary" name="ubah"><i class="glyphicon glyphicon-saved"></i>Simpan</button>
 </form>
 <br><br>
 <?php
 
 if (isset($_POST['ubah'])) {
-    if ($_POST['password'] == "") {
-        $password = $row['password'];
-    } else {
-        $password = $_POST['password'];
-    }
+    $password = empty($_POST['password']) ? $row['password'] : addslashes($_POST['password']);
+    $nip = addslashes($_POST['nip']);
+    $nama = addslashes($_POST['nama']);
+    $email = addslashes($_POST['email']);
+    $jabatan = addslashes($_POST['jabatan']);
+    $telepon = addslashes($_POST['telepon']);
+    $alamat = addslashes($_POST['alamat']);
+
     $namafoto = $_FILES['foto']['name'];
     $lokasifoto = $_FILES['foto']['tmp_name'];
     if (!empty($lokasifoto)) {
         move_uploaded_file($lokasifoto, "../foto/$namafoto");
-        $koneksi->query("UPDATE pengguna SET password='$password',nip='$_POST[nip]',nama='$_POST[nama]',email='$_POST[email]',jabatan='$_POST[jabatan]',fotoprofil='$namafoto', telepon='$_POST[telepon]', alamat='$_POST[alamat]' WHERE id='$_GET[id]'") or die(mysqli_error($koneksi));
+        $koneksi->query("UPDATE pengguna SET password='$password',nip='$nip',nama='$nama',email='$email',jabatan='$jabatan',fotoprofil='$namafoto', telepon='$telepon', alamat='$alamat' WHERE id='" . addslashes($_GET['id']) . "'") or die(mysqli_error($koneksi));
     } else {
-        $koneksi->query("UPDATE pengguna SET password='$password',nip='$_POST[nip]',nama='$_POST[nama]', email='$_POST[email]',jabatan='$_POST[jabatan]', telepon='$_POST[telepon]', alamat='$_POST[alamat]' WHERE id='$_GET[id]'") or die(mysqli_error($koneksi));
+        $koneksi->query("UPDATE pengguna SET password='$password',nip='$nip',nama='$nama',email='$email',jabatan='$jabatan',telepon='$telepon',alamat='$alamat' WHERE id='" . addslashes($_GET['id']) . "'") or die(mysqli_error($koneksi));
     }
-    echo "<script>alert('Data Pegawai Berhasil Di Ubah');</script>";
+    echo "<script>alert('Data Mahasiswa Berhasil Di Ubah');</script>";
     echo "<script>location='index.php?halaman=mahasiswadaftar';</script>";
 }
 ?>
